6af4221d1e6e94487ee59b1f6a0cf825c869ceba
Brandon Zhu <brandonzhu123@gmail.com>
2024-06-05 12:01:46 -0400

    Made config arguments private

--- src/evagram_input/dbconfig/dbconfig.py
+++ src/evagram_input/dbconfig/dbconfig.py
@@ -1,24 +1,24 @@
-from . import config
-from . import test_config
+import config
+import test_config
 
 
 class DatabaseConfiguration:
     """The database configuration object configures keyword arguments for the evagram database
     connection based on the current stage of the application development life cycle."""
     def __init__(self, test_local=False):
-        self.username = config.user
-        self.dbname = config.dbname
-        self.host = config.host
+        self._username = config.user
+        self._dbname = config.dbname
+        self._host = config.host
 
         if test_local:
-            self.username = test_config.user
-            self.dbname = test_config.dbname
-            self.host = test_config.host
+            self._username = test_config.user
+            self._dbname = test_config.dbname
+            self._host = test_config.host
 
     def get_db_parameters(self):
         arguments = {
-            "dbname": self.dbname,
-            "user": self.username,
-            "host": self.host
+            "dbname": self._dbname,
+            "user": self._username,
+            "host": self._host
         }
         return arguments
